0ad3130b1e0a52a47209ec5fe9bc3efe48d501eb
ajksl2356 <ajksl2356@gmail.com>
2024-03-15 11:19:47 +0900

    ..

--- app/src/main/java/com/lym2024/newtptest/activities/MainActivity.kt
+++ app/src/main/java/com/lym2024/newtptest/activities/MainActivity.kt
@@ -40,16 +40,18 @@ import retrofit2.Callback
 import retrofit2.Response
 //abaad2a1-e5ac-417d-989d-b5a25ad7bbb3   서비스키
 
+private fun <T> Call<T>.enqueue(callback: Callback<Search>) {
+
+}
+
 class MainActivity : AppCompatActivity() {
-    var searchQuery: String? = null
+    var searchQuery: String = "연극"
     var myLocation: Location? = null
     var datainfo: Data? = null
-    var search: Search? = null
+    var search: Search?= null
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) } // [ Google Fused Location API 사용  : play-services-location ]
     val locationProviderClient: FusedLocationProviderClient by lazy {
-        LocationServices.getFusedLocationProviderClient(
-            this
-        )
+        LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,22 +61,24 @@ class MainActivity : AppCompatActivity() {
             .commit() // bnv의 선택에 따라 Fragment를 동적으로 교체.
         binding.bnv.setOnItemSelectedListener {
             when (it.itemId) {
-                R.id.menu_bnv_upload -> supportFragmentManager.beginTransaction()
-                    .replace(R.id.container_fragment, PlaceListFragment()).commit()
-
-                R.id.menu_bnv_map -> supportFragmentManager.beginTransaction()
-                    .replace(R.id.container_fragment, PlaceMapFragment()).commit()
-
-                R.id.menu_bnv_girock -> supportFragmentManager.beginTransaction()
-                    .replace(R.id.container_fragment, PlaceFavorFragment()).commit()
+                R.id.menu_bnv_upload -> supportFragmentManager.beginTransaction().replace(R.id.container_fragment, PlaceListFragment()).commit()
+                R.id.menu_bnv_map -> supportFragmentManager.beginTransaction().replace(R.id.container_fragment, PlaceMapFragment()).commit()
+                R.id.menu_bnv_girock -> supportFragmentManager.beginTransaction().replace(R.id.container_fragment, PlaceFavorFragment()).commit()
             }
+
             true
+//            binding.bnv.background = null
+//            binding.etSearch.setOnEditorActionListener { v, actionId, event ->
+//                searchQuery = binding.etSearch.text.toString()
+//                searchPlaces()
+//
+//
+//
+//                false
+//            }
         }   //레트로핏을 이용한 REST API 작업 수행 - GET 방식
-        val retrofit =
-            RetrofitHelper.getRetrofitInstance("http://api.kcisa.kr/openapi/")
-
+        val retrofit = RetrofitHelper.getRetrofitInstance("http://api.kcisa.kr/openapi/")
         val retrofitApiService = retrofit.create(RetrofitApiService::class.java)
-
         val call = retrofitApiService.getDatainfo("abaad2a1-e5ac-417d-989d-b5a25ad7bbb3", "10", "1")
         call.enqueue(object : Callback<Data>{
             override fun onResponse(call: Call<Data>, response: Response<Data>) {
@@ -91,5 +95,34 @@ class MainActivity : AppCompatActivity() {
 
         })
     }
-}
+    // 카카오 로컬 검색 API를 활용하여 키워드로 장소를 검색하는 기능 메소드
+private fun searchPlaces() {
+        Toast.makeText(
+            this,
+            "$searchQuery\\n${myLocation?.latitude},${myLocation?.longitude}\"",
+            Toast.LENGTH_SHORT
+        ).show()
+        val retrofit = RetrofitHelper.getRetrofitInstance("https://dapi.kakao.com")
+        val retrofitApiService = retrofit.create(RetrofitApiService::class.java)
+
+        val call = retrofitApiService.searchPlace(
+            searchQuery,
+            myLocation?.longitude.toString(),
+            myLocation?.latitude.toString()
+        )
+        call.enqueue(object : Callback<Search> {
+            override fun onResponse(call: Call<Search>, response: Response<Search>) {
+                search = response.body()
+
+                val meta: PlaceMeta? = search?.meta
+                val document: List<Place>? = search?.documents
+                binding.bnv.selectedItemId = R.id.menu_bnv_upload
+            }
+
+            override fun onFailure(call: Call<Search>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "서버 오류가 있습니다.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}// main Activity.....
 
