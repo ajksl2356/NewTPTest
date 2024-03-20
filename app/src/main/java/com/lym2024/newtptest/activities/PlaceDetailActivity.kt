dd17e6a2347be919749f40bc2deefe3e8d37c25e
ajksl2356 <ajksl2356@gmail.com>
2024-03-20 09:52:48 +0900

    03.20.대량의데이터 페이징(나눠서 가져오는작업) 작업시작점

--- app/src/main/java/com/lym2024/newtptest/activities/PlaceDetailActivity.kt
+++ app/src/main/java/com/lym2024/newtptest/activities/PlaceDetailActivity.kt
@@ -1,14 +1,34 @@
 package com.lym2024.newtptest.activities
 
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
+import com.google.gson.Gson
 import com.lym2024.newtptest.R
+import com.lym2024.newtptest.data.Title
 import com.lym2024.newtptest.databinding.ActivityPlaceDetailBinding
 
 class PlaceDetailActivity : AppCompatActivity() {
     private val binding by lazy { ActivityPlaceDetailBinding.inflate(layoutInflater) }
+    private lateinit var title :Title
+    private lateinit var db : SQLiteDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_place_detail)
+        setContentView(binding.root)
+        val s : String? = intent.getStringExtra("url")
+        s?.also {
+
+            title = Gson().fromJson(it, Title::class.java)
+            binding.wv.webViewClient = WebViewClient() // 현재 웹뷰안에서 웹문서를 열리도록..
+            binding.wv.webChromeClient = WebChromeClient() // 웹문서안에서 다이얼로그나 팝업같은 것들이 발동하도록..
+            binding.wv.settings.javaScriptEnabled = true // 웹뷰는 기본적으로 보안문제로 JS 동작을 막아놓았기에.. 허용 하도록..
+            binding.wv.loadUrl(title.url)
+        }
+        db = openOrCreateDatabase("url", MODE_PRIVATE, null)
+
+        // "favor"라는 이름의 표(테이블) 만들기 - SQL 쿼리문을 사용하기.. CRUD 작업수행
+        db.execSQL("CREATE TABLE IF NOT EXISTS favor(id TEXT PRIMARY KEY, title TEXT, type TEXT, contactPoint TEXT, url TEXT )")
     }
 }
--- app/src/main/java/com/lym2024/newtptest/data/Data.kt
+++ app/src/main/java/com/lym2024/newtptest/data/Data.kt
@@ -6,13 +6,13 @@ data class ResultCode(var resultCode:String,var resultMsg:String)
 data class Items(var items:Item,val numOfRows:String,var pageNo:String,var totalCount:String )
 data class Item(var item:List<Title>)
 data class Title(
-    var title:String,
-    var type:String,
+    var title:String,           //
+    var type:String,            //
     var period:String,
     var eventPeriod:String,
     var charge:String,
-    var contactPoint:String,
-    var url:String,
+    var contactPoint:String,    //
+    var url:String,             //
     var imageObject:String,
     var description:String,
     var viewCount:String
