b195ead23f86eb258926cae343b318dbb5d76971
ajksl2356 <ajksl2356@gmail.com>
2024-03-20 17:33:37 +0900

    03.20. 디테일프레그먼트 데이터베이스 테이블 이름 바꿈

--- app/src/main/java/com/lym2024/newtptest/activities/PlaceDetailActivity.kt
+++ app/src/main/java/com/lym2024/newtptest/activities/PlaceDetailActivity.kt
@@ -1,5 +1,6 @@
 package com.lym2024.newtptest.activities
 
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,6 +13,7 @@ import com.lym2024.newtptest.databinding.ActivityPlaceDetailBinding
 
 class PlaceDetailActivity : AppCompatActivity() {
     private val binding by lazy { ActivityPlaceDetailBinding.inflate(layoutInflater) }
+    private var isFavorite = false
     private lateinit var title :Title
     private lateinit var db : SQLiteDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +21,6 @@ class PlaceDetailActivity : AppCompatActivity() {
         setContentView(binding.root)
         val s : String? = intent.getStringExtra("url")
         s?.also {
-
             title = Gson().fromJson(it, Title::class.java)
             binding.wv.webViewClient = WebViewClient() // 현재 웹뷰안에서 웹문서를 열리도록..
             binding.wv.webChromeClient = WebChromeClient() // 웹문서안에서 다이얼로그나 팝업같은 것들이 발동하도록..
@@ -29,6 +30,30 @@ class PlaceDetailActivity : AppCompatActivity() {
         db = openOrCreateDatabase("url", MODE_PRIVATE, null)
 
         // "favor"라는 이름의 표(테이블) 만들기 - SQL 쿼리문을 사용하기.. CRUD 작업수행
-        db.execSQL("CREATE TABLE IF NOT EXISTS favor(id TEXT PRIMARY KEY, title TEXT, type TEXT, contactPoint TEXT, url TEXT )")
+        db.execSQL("CREATE TABLE IF NOT EXISTS favor (id TEXT PRIMARY KEY, title TEXT, type TEXT, contactPoint TEXT, url TEXT )")
+
+        isFavorite = checkFavorite()
+        if (isFavorite) binding.fabFavor.setImageResource(R.drawable.baseline_favorite_24)
+        else binding.fabFavor.setImageResource(R.drawable.baseline_favorite_border_24)
+
+        binding.fabFavor.setOnClickListener {
+            if (isFavorite){
+                title.title
+            }
+        }
     }
+    private fun checkFavorite() : Boolean {
+        title.apply {
+            val cursor : Cursor =db.rawQuery("SELECT * FROM favor WHERE title =?", arrayOf(title) )
+            if(cursor.count>0)return true
+
+        }
+        return false
+    }
+
+    override fun onBackPressed() {
+        if (binding.wv.canGoBack())binding.wv.goBack()
+        super.onBackPressed()
+    }
+
 }
