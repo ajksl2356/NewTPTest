097b367bf82e34692b4e64e0eedf521b0b79c962
ajksl2356 <ajksl2356@gmail.com>
2024-03-20 17:34:20 +0900

    03.20. favor프레그먼트 데이터베이스 테이블 이름 바꿈

--- app/src/main/java/com/lym2024/newtptest/fragments/PlaceFavorFragment.kt
+++ app/src/main/java/com/lym2024/newtptest/fragments/PlaceFavorFragment.kt
@@ -31,36 +31,22 @@ class PlaceFavorFragment : Fragment() {
         cursor?.apply {
             moveToFirst()
             val titleList : MutableList<Title> = mutableListOf()
-            val title : String = getString(0)             // 제목
-            val type : String = getString(1)             // 기간
-            val period : String = getString(2)           // 기간
-            val eventPeriod : String  = getString(3)     // 장소
-            val charge : String = getString(4)           // 금액
-            val contactPoint : String  = getString(5)    // 문의 안내
-            val url : String  = getString(6)             // URL
-            val imageObject : String   = getString(7)    // 이미지 썸네일
-            val description : String  = getString(8)     // 설명
-            val viewCount : String = getString(9)
+            for( i in 0 until count ) {
+                val title: String = getString(0)             // 제목
+                val type: String = getString(1)             // 기간
+                val period: String = getString(2)           // 기간
+                val eventPeriod: String = getString(3)     // 장소
+                val charge: String = getString(4)           // 금액
+                val contactPoint: String = getString(5)    // 문의 안내
+                val url: String = getString(6)             // URL
+                val imageObject: String = getString(7)    // 이미지 썸네일
+                val description: String = getString(8)     // 설명
+                val viewCount: String = getString(9)
+                val title1: Title = Title(title, type, period, eventPeriod, charge, contactPoint, url, imageObject, description, viewCount)
+                titleList.add(title1)
+                moveToNext()
+            }
+            binding.recyclerView.adapter = MyAdapter(requireContext(),titleList)
         }
-//        val db = requireContext().openOrCreateDatabase("place", Activity.MODE_PRIVATE, null)
-//        val cursor = db.rawQuery("SELECT * FROM favor",null)
-//        cursor?.apply {
-//            moveToFirst()
-//            val placeList : MutableList<Place> = mutableListOf()
-//            val id : String = getString(0)
-//            val place_name : String = getString(1)
-//            val category_name : String = getString(2)
-//            val phone : String = getString(3)
-//            val address_name : String = getString(4)
-//            val road_address_name : String = getString(5)
-//            val x : String = getString(6)
-//            val y : String = getString(7)
-//            val place_url : String = getString(8)
-//            val distance : String = getString(9)
-//            val place : Place = Place(id, place_name, category_name, phone, address_name, road_address_name, x, y, place_url, distance)
-//            placeList.add(place)
-//            moveToNext()
-//            binding.recyclerView.adapter = MyAdapter(requireContext(), placeList)
-//        }
     }
 }
