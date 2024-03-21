b64eb13c170681ebe67ddb92f185fb60264789e5
ajksl2356 <ajksl2356@gmail.com>
2024-03-21 17:50:28 +0900

    03.21. 플레이스 디테일 액티비티 클래스 완성

--- app/src/main/java/com/lym2024/newtptest/activities/PlaceDetailActivity.kt
+++ app/src/main/java/com/lym2024/newtptest/activities/PlaceDetailActivity.kt
@@ -1,17 +1,21 @@
 package com.lym2024.newtptest.activities
 
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
+import android.widget.Toast
 import com.google.gson.Gson
+import com.lym2024.newtptest.R
 import com.lym2024.newtptest.data.Place
 import com.lym2024.newtptest.databinding.ActivityPlaceDetailBinding
 
 class PlaceDetailActivity : AppCompatActivity() {
     private val binding by lazy { ActivityPlaceDetailBinding.inflate(layoutInflater) }
     private lateinit var place : Place
+    private var isFavorite = false
     private lateinit var db2 : SQLiteDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +29,44 @@ class PlaceDetailActivity : AppCompatActivity() {
             binding.wv2.loadUrl(place.place_url)
         }
         db2 = openOrCreateDatabase("place", MODE_PRIVATE,null)
-        db2.execSQL("CREATE TABLE IF NOT EXISTS favor(id TEXT PRIMARY KEY, place_name TEXT, category_name TEXT, phone TEXT, address_name TEXT, road_address_name TEXT, x TEXT, y TEXT, place_url TEXT, distance TEXT )")
-
+        db2.execSQL("CREATE TABLE IF NOT EXISTS good(id TEXT PRIMARY KEY, place_name TEXT, category_name TEXT, phone TEXT, address_name TEXT, road_address_name TEXT, x TEXT, y TEXT, place_url TEXT, distance TEXT )")
+        isFavorite = checkFavorite()
+        if (isFavorite) binding.fabFavor.setImageResource(R.drawable.baseline_favorite_24)
+        else binding.fabFavor.setImageResource(R.drawable.baseline_favorite_border_24)
+        // 찜 버튼 클릭 처리
+        binding.fabFavor.setOnClickListener {
+            if (isFavorite){
+                // 찜 DB의 데이터를 삭제
+                place.apply {
+                    db2.execSQL("DELETE FROM good WHERE id = ?", arrayOf(id))
+                }
+                Toast.makeText(this, "찜 목록에서 제거되었습니다.", Toast.LENGTH_SHORT).show()
+            }else{
+                // 찜 DB에 데이터를 저장
+                place.apply {
+                    place.apply {
+                        db2.execSQL("INSERT INTO good VALUES('$id','$place_name','$category_name','$phone','$address_name','$road_address_name','$x','$y','$place_url','$distance')")
+                    }
+                }
+                Toast.makeText(this, "찜 목록에 추가되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+            isFavorite =! isFavorite
+            if (isFavorite) binding.fabFavor.setImageResource(R.drawable.baseline_favorite_24)
+            else binding.fabFavor.setImageResource(R.drawable.baseline_favorite_border_24)
+        }
     }
+    private fun checkFavorite() : Boolean {
+        place.apply {
+            val cursor : Cursor = db2.rawQuery("SELECT * FROM good WHERE id =?", arrayOf(id))
+            if(cursor.count>0)return true
+        }
+        return false
+    }
+
+    override fun onBackPressed() {
+        if (binding.wv2.canGoBack()) binding.wv2.goBack()
+        super.onBackPressed()
+    }
+
+
 }
