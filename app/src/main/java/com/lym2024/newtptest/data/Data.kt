419df27c19fde416590d749530cb3819938d4d10
ajksl2356 <ajksl2356@gmail.com>
2024-03-21 09:50:35 +0900

    03.21. 데이터클래스 장소 누락된거 추가함

--- app/src/main/java/com/lym2024/newtptest/data/Data.kt
+++ app/src/main/java/com/lym2024/newtptest/data/Data.kt
@@ -6,14 +6,15 @@ data class ResultCode(var resultCode:String,var resultMsg:String)
 data class Items(var items:Item,val numOfRows:String,var pageNo:String,var totalCount:String )
 data class Item(var item:List<Title>)
 data class Title(
-    var title:String,           // 제목
-    var type:String,            // 기간
-    var period:String,          // 기간
-    var eventPeriod:String,     // 장소
-    var charge:String,          // 금액
-    var contactPoint:String,    // 문의 안내
-    var url:String,             // URL
-    var imageObject:String,     // 이미지 썸네일
-    var description:String,     // 설명
-    var viewCount:String        // 조회수
+    var title : String,           // 제목
+    var type : String,            // 기간
+    var period : String,          // 기간
+    var eventPeriod : String,     // 시간
+    var eventSite : String,       // 장소
+    var charge : String,          // 금액
+    var contactPoint : String,    // 문의 안내
+    var url : String,             // URL
+    var imageObject : String,     // 이미지 썸네일
+    var description : String,     // 설명
+    var viewCount : String        // 조회수
 )
