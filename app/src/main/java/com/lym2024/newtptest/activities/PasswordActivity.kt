4aea6f6da1d44576e44ccfb6f711fed01b90a374
ajksl2356 <ajksl2356@gmail.com>
2024-03-25 11:04:31 +0900

    03.25. 앱 이름이랑 사진 바꿈 이메일 데이터클래스 추가함 패스워드 엑티비티 만듦

---
+++ app/src/main/java/com/lym2024/newtptest/activities/PasswordActivity.kt
@@ -0,0 +1,40 @@
+package com.lym2024.newtptest.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.lym2024.newtptest.R
+import com.lym2024.newtptest.data.data
+import com.lym2024.newtptest.databinding.ActivityPasswordBinding
+
+class PasswordActivity : AppCompatActivity() {
+    val binding by lazy { ActivityPasswordBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnSave.setOnClickListener { clickSave() }
+        finish()
+    }
+    private fun clickSave() {
+        val newPassword: String = binding.inputLayoutPassword.editText!!.text.toString()
+        val userEmail: String = binding.inputLayoutEmail.editText!!.text.toString()
+        val data:MutableMap<String,Any> = mutableMapOf()
+
+        data["email"] = userEmail
+        data["password"] = newPassword
+
+        val userRef: CollectionReference = Firebase.firestore.collection("emailUsers")
+        val user: data = data(userEmail, newPassword)
+        userRef.document().set(user).addOnSuccessListener {
+            Toast.makeText(this, "save success", Toast.LENGTH_SHORT).show() }
+
+        binding.btnSave.setOnClickListener {
+            startActivity(Intent(this, EmailLoginActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/lym2024/newtptest/data/Email_data.kt
@@ -0,0 +1,3 @@
+package com.lym2024.newtptest.data
+
+data class data(var email : String, var password : String )
