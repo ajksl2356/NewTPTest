24a61a1555bba655f530ac740aeac43dd87ff2f3
ajksl2356 <ajksl2356@gmail.com>
2024-03-25 17:47:11 +0900

    03.25.  이메일 이랑 비번 틀리면 로그인 안되게함 코끼리 파이어스토어 서비스제이슨 따옴 XML 수정

--- app/src/main/java/com/lym2024/newtptest/activities/EmailLoginActivity.kt
+++ app/src/main/java/com/lym2024/newtptest/activities/EmailLoginActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import com.google.android.gms.tasks.OnFailureListener
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -16,18 +19,37 @@ class EmailLoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.toolbar.setNavigationOnClickListener { finish() }
-        binding.btnSignin.setOnClickListener { clickSignup() }
+        binding.btnLogin.setOnClickListener { clickLogin() }
         binding.btnChange.setOnClickListener { clickChange() }
     }
-    private fun clickSignup(){
+
+    private fun clickLogin() {
         val email = binding.inputLayoutEmail.editText!!.text.toString()
         val password = binding.inputLayoutPassword.editText!!.text.toString()
-        val userRef : CollectionReference = Firebase.firestore.collection("emailUser")
+        val userRef: CollectionReference = Firebase.firestore.collection("emailUsers")
+
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            userRef.whereEqualTo("email", email)
+                .whereEqualTo("password", password)
+                .get()
+                .addOnSuccessListener { documents ->
+                    if (!documents.isEmpty) {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(this, "이메일 또는 비밀번호가 올바르지 않습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener(object : OnFailureListener {
+                    override fun onFailure(exception: Exception) {
+                        Toast.makeText(this@EmailLoginActivity, "로그인 중 오류가 발생했습니다: ${exception.message}", Toast.LENGTH_SHORT).show()
+                    }
+                })
+        } else {
+            Toast.makeText(this, "이메일 또는 비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show()
+        }
     }
     private fun clickChange(){
-        binding.btnChange.setOnClickListener {
-            startActivity(Intent(this@EmailLoginActivity, PasswordActivity::class.java))
-            finish()
-        }
+        startActivity(Intent(this,PasswordActivity::class.java))
     }
 }
